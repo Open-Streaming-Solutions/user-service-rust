24144ac42f649bcb41546e4a28204fa583fee68f
Eslrusgag <warkar09@gmail.com>
2024-07-28 00:25:52 +0300

    added update_user_by_id and update_user_by_nickname

--- server/src/adapters/repo/mod.rs
+++ server/src/adapters/repo/mod.rs
@@ -23,6 +23,10 @@ impl UserRepository for InternalRepository {
         self.storage.insert(user.id, user);
     }
 
+    async fn get_all_users(&self) -> Vec<User> {
+        self.storage.iter().map(|kv| kv.value().clone()).collect()
+    }
+
     async fn get_user(&self, user_id: &Uuid) -> Option<User> {
         self.storage.get(user_id).map(|user| user.clone())
     }
@@ -36,8 +40,16 @@ impl UserRepository for InternalRepository {
             .find(|kv| kv.value().user_name == user_name)
             .map(|kv| *kv.key())
     }
-
-    async fn get_all_users(&self) -> Vec<User> {
-        self.storage.iter().map(|kv| kv.value().clone()).collect()
+    async fn update_user_by_id(&self, user_id: &Uuid, updated_user: User) -> Option<()> {
+        if self.storage.contains_key(user_id) {
+            self.storage.insert(*user_id, updated_user);
+            Some(())
+        } else {
+            None
+        }
+    }
+    async fn update_user_by_nickname(&self, nick_name: &str, updated_user: User) -> Option<()> {
+        let user_id = self.get_user_id_by_nickname(nick_name).await?;
+        self.update_user_by_id(&user_id, updated_user).await
     }
 }
