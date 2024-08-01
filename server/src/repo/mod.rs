c79396b386a35db34a8885ebfd3850451d462a7d
Eslrusgag <warkar09@gmail.com>
2024-08-01 15:14:16 +0300

    Rename methods

--- server/src/repo/mod.rs
+++ server/src/repo/mod.rs
@@ -1,9 +1,7 @@
 use crate::types::User;
 use tonic::async_trait;
 use uuid::Uuid;
-
 mod database;
-pub mod internal;
 
 use crate::errors::RepoError;
 
@@ -14,7 +12,7 @@ pub trait UserRepository: Send + Sync {
     async fn get_all_users(&self) -> Result<Vec<User>, RepoError>;
     async fn get_user(&self, user_id: &Uuid) -> Result<Option<User>, RepoError>;
     async fn get_user_id(&self, user_id: &Uuid) -> Result<Option<Uuid>, RepoError>;
-    async fn get_user_id_by_nickname(&self, user_name: &str) -> Result<Option<Uuid>, RepoError>;
+    async fn get_user_id_by_username(&self, user_name: &str) -> Result<Option<Uuid>, RepoError>;
     async fn update_user_by_id(&self, user_id: &Uuid, updated_user: User, ) -> Result<Option<()>, RepoError>;
-    async fn update_user_by_nickname(&self, nick_name: &str, updated_user: User) -> Result<Option<()>, RepoError>;
+    async fn update_user_by_username(&self, nick_name: &str, updated_user: User) -> Result<Option<()>, RepoError>;
 }
