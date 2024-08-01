0f9df0359274c5cbc715e8f165c830a177911043
Eslrusgag <warkar09@gmail.com>
2024-08-01 10:57:05 +0300

    Add getter methods for User fields

--- server/src/types.rs
+++ server/src/types.rs
@@ -1,11 +1,33 @@
 use crate::adapters::schema::users;
 use diesel::{Insertable, Queryable};
 use uuid::Uuid;
+use crate::adapters::schema::users::{email, id, username};
 
-#[derive(Debug, Clone, Queryable, Insertable)]
+#[derive(Debug, Clone, Queryable, Insertable, PartialEq)]
 #[diesel(table_name = users)]
 pub struct User {
-    pub id: Uuid,
-    pub username: String,
-    pub email: String,
+    id: Uuid,
+    username: String,
+    email: String,
+}
+
+impl User {
+    pub async fn new(id: Uuid, username: String, email: String) -> Self {
+        User {
+            id,
+            username,
+            email,
+        }
+    }
+    pub fn get_id(&self) -> Uuid {
+        self.id
+    }
+
+    pub fn get_username(&self) -> &str {
+        &self.username
+    }
+
+    pub fn get_email(&self) -> &str {
+        &self.email
+    }
 }
